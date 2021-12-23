e8c27c8c7cf66628d873042737c6943c16390227
IRHM <37304121+IRHM@users.noreply.github.com>
2021-12-23 00:49:29 +0000

    Add dockerfile and docker-compose files
    and move to using import instead of require

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-require("dotenv").config();
-const puppeteer = require("puppeteer");
-const nodemailer = require("nodemailer");
-const chalk = import("chalk");
-const path = require("path");
+import dotenv from "dotenv";
+import puppeteer from "puppeteer";
+import nodemailer from "nodemailer";
+import path from "path";
+import chalk from "chalk";
 
 function sleep(hours) {
   const ms = hours * 60000 * 60;
@@ -10,37 +10,44 @@ function sleep(hours) {
 }
 
 (async () => {
+  dotenv.config();
+
   while (true) {
-    const browser = await puppeteer.launch({ headless: true });
-    const page = await browser.newPage();
-    await page.setUserAgent(
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"
-    );
-    await page.setViewport({ width: 1280, height: 1500 });
-    await page.goto("https://www.just-eat.co.uk/courier");
-
-    // Wait for all elements required
-    const signupForm = await page.waitForSelector("#signupForm");
-
-    // Select items in form that make vehicle menu popup
-    await page.select("select[name=city_driver_operates]", process.env.CITY_TO_CHECK);
-    await page.select("select[name=age_UK]", "Between 18 and 20 years old");
-
-    // Screenshot signup form
-    // signupForm.screenshot, would show a white box in image, so clipping manually instead.
-    const screenshotOut = path.resolve("latest.png");
-    const sfCoords = await signupForm.boundingBox();
-    await page.screenshot({
-      path: screenshotOut,
-      clip: { x: sfCoords.x, y: sfCoords.y, width: sfCoords.width, height: sfCoords.height }
-    });
-
-    await browser.close();
-
-    notify(screenshotOut);
-
     const sleepHrs = 5;
-    console.log(`Sent notification, sleeping for 5 hours.`);
+
+    try {
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+      await page.setUserAgent(
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"
+      );
+      await page.setViewport({ width: 1280, height: 1500 });
+      await page.goto("https://www.just-eat.co.uk/courier");
+
+      // Wait for all elements required
+      const signupForm = await page.waitForSelector("#signupForm");
+
+      // Select items in form that make vehicle menu popup
+      await page.select("select[name=city_driver_operates]", process.env.CITY_TO_CHECK);
+      await page.select("select[name=age_UK]", "Between 18 and 20 years old");
+
+      // Screenshot signup form
+      // signupForm.screenshot, would show a white box in image, so clipping manually instead.
+      const screenshotOut = path.resolve("latest.png");
+      const sfCoords = await signupForm.boundingBox();
+      await page.screenshot({
+        path: screenshotOut,
+        clip: { x: sfCoords.x, y: sfCoords.y, width: sfCoords.width, height: sfCoords.height }
+      });
+
+      await browser.close();
+
+      notify(screenshotOut);
+
+      console.log(chalk.green.bold(`Sending notification, sleeping for ${sleepHrs} hours...`));
+    } catch (err) {
+      console.log("An Error Occurred!", err);
+    }
 
     await sleep(sleepHrs);
   }
@@ -58,14 +65,14 @@ function notify(ss) {
   });
 
   transporter.verify((err, success) => {
-    if (err) console.log(chalk.red("Error connecting to mail server: ", err.message));
+    if (err)
+      console.log(
+        chalk.red(`Error connecting to mail server for user ${process.env.MAIL_USER}: `, JSON.stringify(err))
+      );
   });
 
   const ssFileName = path.basename(ss);
   const ssCid = ssFileName + "@";
-
-  console.log(ssFileName, ssCid);
-
   const msg = {
     from: process.env.MAIL_USER,
     to: process.env.MAIL_TO,
